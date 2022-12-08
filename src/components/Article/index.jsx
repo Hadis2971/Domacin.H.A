6243ca203585ecb6302173c80cb8d49570acd5d3
Hadis1990 <hsteammsteam@gmail.com>
2022-12-08 19:36:36 +0100

    update articles component for different pages

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -1,20 +1,30 @@
+import styled from "styled-components";
+
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 
 import "./index.scss";
 
-function Article({ imgSrc, title, description }) {
+const ArticleBody = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  padding: 10px;
+  flex-direction: ${(props) => (props.column ? "column" : "initial")};
+`;
+
+function Article({ imgSrc, title, description, column }) {
   return (
     <div className="article-container">
-      <div className="article-body">
+      <ArticleBody column={column}>
         <div className="image-container">
-          <Image src={imgSrc} fluid rounded className="image" />
+          <Image src={imgSrc} fluidrounded className="image" />
         </div>
         <div>
           <h5>{title}</h5>
           <p>{description}</p>
         </div>
-      </div>
+      </ArticleBody>
       <div className="article-footer">
         <Button>Saznaj Vise</Button>
       </div>
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import {
   PageContentContainer,
   PageTitleDescription,
   Product,
+  Article,
 } from "../../components";
 
 import "./index.scss";
@@ -59,6 +60,39 @@ export default () => {
           description="Čarobno srce – Unikatni pokloni – handmade je divan trik koji mami osmijeh i budi najljepše osjećaje. Posebno dizajniran za sve one koji vole ili su voljeni, a sa ciljem da zaustavi govor i izazove lavinu poljubaca i zagrljaja. Miris čokolade povećava theta moždane valove koji izazivaju opuštanje. Zato opustite se i naručite slatki grijeh!"
           price={50}
         />
+      </div>
+
+      <div id="home-page-articles-list">
+        <PageTitleDescription
+          title="Savjeti o zdravlju, domaćinstvu i recepti"
+          description="Domaćinski savjeti o zdravlju, recepti, uradi sam projekti, poljoprivreda i sve za dom"
+          bgColor=""
+          titleColor="#3c4858"
+          descriptionColor="#999999"
+        />
+
+        <div id="home-page-articles-container">
+          <Article
+            imgSrc="https://t4.ftcdn.net/jpg/03/00/85/23/360_F_300852364_qSrtNxY6pokaVR7er7knpb8AyYJSxtUd.jpg"
+            title="Najnovije vijseti"
+            description="Najbolji izbor nameštaja za Vas!  Pre nego što se odlučite za odgovarajuću vrstu nameštaja, važno je da sagledate prostor koji imate na raspolaganju i šta želite odabirom odgovarajućeg nameštaja da postignete.  Nameštaj je ogledalo doma i bitno je da se"
+            column
+          />
+
+          <Article
+            imgSrc="https://t4.ftcdn.net/jpg/03/00/85/23/360_F_300852364_qSrtNxY6pokaVR7er7knpb8AyYJSxtUd.jpg"
+            title="Najnovije vijseti"
+            description="Najbolji izbor nameštaja za Vas!  Pre nego što se odlučite za odgovarajuću vrstu nameštaja, važno je da sagledate prostor koji imate na raspolaganju i šta želite odabirom odgovarajućeg nameštaja da postignete.  Nameštaj je ogledalo doma i bitno je da se"
+            column
+          />
+
+          <Article
+            imgSrc="https://t4.ftcdn.net/jpg/03/00/85/23/360_F_300852364_qSrtNxY6pokaVR7er7knpb8AyYJSxtUd.jpg"
+            title="Najnovije vijseti"
+            description="Najbolji izbor nameštaja za Vas!  Pre nego što se odlučite za odgovarajuću vrstu nameštaja, važno je da sagledate prostor koji imate na raspolaganju i šta želite odabirom odgovarajućeg nameštaja da postignete.  Nameštaj je ogledalo doma i bitno je da se"
+            column
+          />
+        </div>
       </div>
     </PageContentContainer>
   );
